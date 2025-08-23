9e0114c7123d9e48e1ffa5cd521a406d5502b356
andrekrippahl <andre.krippahl47@gmail.com>
2025-08-24 00:20:35 +0100

    pequena correção nos erros

--- app/errors/geocoding_failed_error.rb
+++ app/errors/geocoding_failed_error.rb
@@ -1,5 +1,10 @@
 class GeocodingFailedError < DomainError
   def initialize(context: nil)
-    super(message: "Could not resolve location.", code: "geocoding_failed", http_status: 422, context: context)
+    super(
+      message: "Could not resolve location.",
+      code: "geocoding_failed",
+      http_status: 422,
+      context: context
+    )
   end
 end
